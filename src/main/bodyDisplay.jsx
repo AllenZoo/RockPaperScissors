39b43f0a2a49ebaa13a517584e7e1f69396f620c
AllenZoo <allenzhu2000@hotmail.com>
2022-10-04 16:14:18 -0700

    Added rules button

--- src/main/bodyDisplay.jsx
+++ src/main/bodyDisplay.jsx
@@ -1,8 +1,18 @@
 import React, { useState } from "react";
 import WeaponSelector from "./game/WeaponSelector";
+import ".././styles/body.css";
 
 function BodyDisplay() {
-  return <WeaponSelector />;
+  return (
+    <div>
+      <div className="body-display">
+        <WeaponSelector />
+      </div>
+      <div>
+        <button className="rules-button">RULES</button>
+      </div>
+    </div>
+  );
 }
 
 export default BodyDisplay;
