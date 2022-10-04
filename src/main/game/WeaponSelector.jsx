008fb6d439eb103c27d7d9c17eb977d448e29725
AllenZoo <allenzhu2000@hotmail.com>
2022-10-04 15:53:43 -0700

    Updated css

--- src/main/game/WeaponSelector.jsx
+++ src/main/game/WeaponSelector.jsx
@@ -4,15 +4,22 @@ import paperImg from "../../images/icon-paper.svg";
 import scissorImg from "../../images/icon-scissors.svg";
 import triangleBg from "../../images/bg-triangle.svg";
 import WeaponIcon from "./components/weaponIcon";
+import "../../styles/selector.css";
 
 function WeaponSelector() {
   return (
     <div>
-      {/* <img src={rockImg} alt="rock-icon"></img>
-      <img src={triangleBg}></img> */}
-      <WeaponIcon iconIMG={rockImg} iconType="rock"></WeaponIcon>
-      <WeaponIcon iconIMG={paperImg} iconType="paper"></WeaponIcon>
-      <WeaponIcon iconIMG={scissorImg} iconType="scissors"></WeaponIcon>
+      <img src={triangleBg} alt="triangle-bg"></img>
+      <button className="icon-button">
+        <WeaponIcon iconIMG={rockImg} iconType="rock"></WeaponIcon>
+      </button>
+
+      <button className="icon-button">
+        <WeaponIcon iconIMG={paperImg} iconType="paper"></WeaponIcon>
+      </button>
+      <button className="icon-button">
+        <WeaponIcon iconIMG={scissorImg} iconType="scissors"></WeaponIcon>
+      </button>
     </div>
   );
 }
