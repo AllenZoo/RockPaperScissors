5ee523956b7bfb8972969e5c8b923b7affaacbbd
AllenZoo <allenzhu2000@hotmail.com>
2022-10-04 18:15:13 -0700

    added rules page

--- src/main/bodyDisplay.jsx
+++ src/main/bodyDisplay.jsx
@@ -1,15 +1,24 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import WeaponSelector from "./game/WeaponSelector";
 import ".././styles/body.css";
+import RulesPage from "../rules/ruleDisplay.jsx/rulesPage";
 
 function BodyDisplay() {
+  //const { mode } = useContext(AppContext);
+
+  const displayRules = () => {};
   return (
     <div>
       <div className="body-display">
         <WeaponSelector />
       </div>
       <div>
-        <button className="rules-button">RULES</button>
+        <RulesPage />
+      </div>
+      <div>
+        <button onClick={displayRules} className="rules-button">
+          RULES
+        </button>
       </div>
     </div>
   );
---
+++ src/rules/ruleDisplay.jsx/rulesPage.jsx
@@ -0,0 +1,11 @@
+import ruleIMG from "../../images/image-rules.svg";
+
+function RulesPage() {
+  return (
+    <div className="rule-page-container">
+      <img src={ruleIMG} />
+    </div>
+  );
+}
+
+export default RulesPage;
