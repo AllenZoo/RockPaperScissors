e772d37bc53eec5f72b7de5b193319be8de9874a
AllenZoo <allenzhu2000@hotmail.com>
2026-01-08 19:47:35 -0800

    [RPS] Added questions

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,3 @@
 export * from "./game";
 export * from "./ai";
+export * from "./questions";
---
+++ src/constants/questions.js
@@ -0,0 +1,8 @@
+questions = [
+  ["Are you going to play rock?", 1],
+  ["Are you going to play paper?", 2],
+  ["Are you going to play scissors?", 3],
+  ["Will you repeat your last move?", 4],
+  ["Are you trying to trick me?", 5],
+];
+export const QUESTIONS = questions;
